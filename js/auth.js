ad748eac9e9b9ef45d870d4cfb3d4129c191ada3
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-04-16 09:55:28 +0200

    Reset inputs value after submit

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,8 @@
 //signup
+const signupForm = document.getElementsByClassName('form-control')[0];
+sigupForm.addEventListener('submit', e => {
+  const email = document.getElementsByClassName('form-control')[0].value;
+  const password = document.getElementsByClassName('form-control')[1].value;
+
+  auth.createUserWithEmailAndPassword();
+});
--- js/index.js
+++ js/index.js
@@ -2,31 +2,33 @@ function loginButtonClicked() {
   let loginFormValue = document.getElementsByClassName('form-control')[0].value;
   let passwordFormValue = document.getElementsByClassName('form-control')[1]
     .value;
-    console.log(loginFormValue);
-    console.log(passwordFormValue);
-};
+  console.log(loginFormValue);
+  console.log(passwordFormValue);
+
+  // Reset inputs
+  document.getElementsByClassName('form-control')[0].value = '';
+  document.getElementsByClassName('form-control')[1].value = '';
+}
 
 function signUpButtonClicked() {
   let passwordFormValue = document.getElementsByClassName('form-control')[1]
     .value;
-  let repeatPasswordFormValue = document.getElementsByClassName('form-control')[2].value;
+  let repeatPasswordFormValue = document.getElementsByClassName(
+    'form-control'
+  )[2].value;
   if (passwordFormValue === repeatPasswordFormValue) {
     console.log('success');
-  }
-  else 
-    console.log("Those passwords didn't match. Try again");
-};
+  } else console.log("Those passwords didn't match. Try again");
+}
 
 function checkIfPasswordsMatches(loginFormValue, passwordFormValue) {
   // let loginFormValue = document.getElementsByClassName('form-control')[0].value;
   // let passwordFormValue = document.getElementsByClassName('form-control')[1].value;
 
-    if (loginFormValue === passwordFormValue) {
-      console.log('success');
-    }
-    else 
-      console.log("Those passwords didn't match. Try again");
-};
+  if (loginFormValue === passwordFormValue) {
+    console.log('success');
+  } else console.log("Those passwords didn't match. Try again");
+}
 
 addEventListener('keyup', function(event) {
   // Number 13 is the "Enter" key on the keyboard
@@ -35,5 +37,3 @@ addEventListener('keyup', function(event) {
     loginButtonClicked();
   }
 });
-
-
