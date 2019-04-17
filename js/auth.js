93c60de1f21e3863bf3acb532fe9ae332e9ebe20
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-04-17 08:17:42 +0200

    Register error handling

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,20 @@
 // get user credentials
-function loginButtonClicked() {
+function signUpButtonClicked() {
   const email = document.getElementsByClassName('form-control')[0].value;
   const password = document.getElementsByClassName('form-control')[1].value;
+  const repeatPassword = document.getElementsByClassName('form-control')[2]
+    .value;
 
-  document.getElementsByClassName('form-control')[0].value = '';
-  document.getElementsByClassName('form-control')[1].value = '';
+  if (password === repeatPassword) {
+    document.getElementsByClassName('form-control')[0].value = '';
+    document.getElementsByClassName('form-control')[1].value = '';
 
-  // signup the user with Firebase
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-  });
+    // signup the user with Firebase
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+      console.log(cred.user);
+    });
+  } else {
+    document.getElementById('error-message').innerHTML =
+      "Those passwords didn't match. Try again";
+  }
 }
