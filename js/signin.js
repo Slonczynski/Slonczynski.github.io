3efe0ae7e4e5f3a23434cf12e5197efff06f2ad1
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-04-22 00:48:31 +0200

    Spinner workaround

--- js/signin.js
+++ js/signin.js
@@ -7,10 +7,11 @@ addEventListener('keyup', function(event) {
 });
 
 function loginButtonClicked() {
-  nodes = document.getElementById('spinner-error-box');
-  if (nodes) {
-    document.getElementById('spinner-error-box').outerHTML = '';
-  }
+  // nodes = document.getElementById('error-messages');
+  // if (nodes) {
+  //   document.getElementById('spinner-error-box').outerHTML = '';
+  // }
+
   const email = document.getElementsByClassName('form-control')[0].value;
   const password = document.getElementsByClassName('form-control')[1].value;
 
@@ -21,29 +22,20 @@ function loginButtonClicked() {
   auth
     .signInWithEmailAndPassword(email, password)
 
-    // let div = document.createElement('spinner');
-    // div
-    //   .setAttribute('class', 'spinner-border')
-
     .then(user => {
       console.log('success');
       window.location.href = 'todo.html';
     })
     .catch(error => {
-      // let el = document.getElementsByClassName('spinner-border');
-
-      // document.getElementById('error-message').removeChild(el);
-
-      // document.getElementById('error-messages').innerHTML = error.message;
-
-      // let elem = document.getElementById('#');
-      // elem.removeChild(elem);
-
+      // let elem = document.getElementById('spinner-error-box');
+      // while (elem[0]) {
+      //   elem[0].parentNode.removeChild(elem[0]);
+      // }
       // Show error to the user
-      let errorMessage = document.createElement('span');
-      node.setAttribute('id', 'error-messages');
-      document
-        .getElementById('spinner-error-box')
-        .appendChild(errorMessage).innerHTML = error.message;
+      // let errorMessage = document.createElement('span');
+      // node.setAttribute('id', 'error-messages');
+      // document
+      //   .getElementById('spinner-error-box')
+      //   .appendChild(errorMessage).innerHTML = error.message;
     });
 }
