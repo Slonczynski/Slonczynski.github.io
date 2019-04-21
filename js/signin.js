c24025a3328b5d35e3eb6ebae180d1150faa60a3
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-04-21 23:56:37 +0200

    Spinner added + error handling improvement

--- js/signin.js
+++ js/signin.js
@@ -7,15 +7,43 @@ addEventListener('keyup', function(event) {
 });
 
 function loginButtonClicked() {
+  nodes = document.getElementById('spinner-error-box');
+  if (nodes) {
+    document.getElementById('spinner-error-box').outerHTML = '';
+  }
   const email = document.getElementsByClassName('form-control')[0].value;
   const password = document.getElementsByClassName('form-control')[1].value;
+
+  let node = document.createElement('div');
+  node.setAttribute('class', 'spinner-border');
+  document.getElementById('spinner-error-box').appendChild(node);
+
   auth
     .signInWithEmailAndPassword(email, password)
+
+    // let div = document.createElement('spinner');
+    // div
+    //   .setAttribute('class', 'spinner-border')
+
     .then(user => {
-      console.log('succes');
+      console.log('success');
       window.location.href = 'todo.html';
     })
     .catch(error => {
-      document.getElementById('error-messages').innerHTML = error.message;
+      // let el = document.getElementsByClassName('spinner-border');
+
+      // document.getElementById('error-message').removeChild(el);
+
+      // document.getElementById('error-messages').innerHTML = error.message;
+
+      // let elem = document.getElementById('#');
+      // elem.removeChild(elem);
+
+      // Show error to the user
+      let errorMessage = document.createElement('span');
+      node.setAttribute('id', 'error-messages');
+      document
+        .getElementById('spinner-error-box')
+        .appendChild(errorMessage).innerHTML = error.message;
     });
 }
--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,7 @@
 const logout = document.querySelector('#logout-button');
 logout.addEventListener('click', e => {
   auth.signOut().then(() => {
+    window.location.href = 'index.html';
     console.log('user signed out');
   });
 });
