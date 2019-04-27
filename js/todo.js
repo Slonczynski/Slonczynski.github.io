3a11cad5e7028036262b0ba7889894d7ba057c00
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-04-28 01:06:37 +0200

    Fix redirection

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,7 @@
 const logout = document.querySelector('#logout-button');
 logout.addEventListener('click', e => {
   auth.signOut().then(() => {
-    window.location.href = 'index.html';
+    window.location.href = '/';
     console.log('user signed out');
   });
 });
